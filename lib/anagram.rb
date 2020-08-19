1ed109b825d7540a94c907e280e53b8c0f52fa93
Brian Donnoe <dunow33@hotmail.com>
2020-08-19 05:18:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,6 +10,10 @@ class Anagram
     words = []
     
     anagrams.each do | aword |
+      puts aword.split.sort
+      puts
+      puts word.split.sort
+      
       if aword.split.sort == word.split.sort
         words << aword
       end
