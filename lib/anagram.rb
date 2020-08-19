e7133f6a69c73d23731a912fc271a886039c3c4a
Brian Donnoe <dunow33@hotmail.com>
2020-08-19 05:11:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     words = []
     
     anagrams.each do | aword |
-      if my_string.chars.sort.join == word
+      if aword.chars.sort.join == word
         words << aword
       end
     end
