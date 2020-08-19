60c29994e44845a030bdf1c9b43ae0aa494b88f4
Brian Donnoe <dunow33@hotmail.com>
2020-08-19 05:06:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     words = []
     
     anagrams.each do | aword |
-      if aword.chars.sort == word
+      if aword.chars.sort_by(&:downcase) == word
         words << aword
       end
     end
