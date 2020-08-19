4d9cd39928236063e3f34fb725ba5dde69883905
Brian Donnoe <dunow33@hotmail.com>
2020-08-19 05:04:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     words = []
     
     anagrams.each do | aword |
-      if aword.chars.sort == word
+      if aword.chars.sort(&:casecmp) == word
         words << aword
       end
     end
